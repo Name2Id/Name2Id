57a1530f629707d270d1442b4ba47b938d9b9514
MyName2Ids <r.gg.3@outlook.com>
2023-04-18 20:45:14 -0600

    definiendo paleta de colores

--- ramiroSbackN/index.php
+++ ramiroSbackN/index.php
@@ -1,7 +1,10 @@
 <?php
     require_once './ramiroSbackN.php';
     $rSbN = new ramiroSbackN;
-    $message = "hello worrld!";
+    #$rSbN->conectar();
+    #$filasAfectadasAlInsertar = $rSbN->nonQuery("INSERT INTO kftable_topics (topic)VALUES('new topic family kids')");
+    #$ultimoIdInsertado = $rSbN->nonQueryId("INSERT INTO kftable_topics (topic)VALUES('new topic family kids')");
+    $message = "hello worrld!<br>filas afectadas al insertar : \$filasAfectadasAlInsertar<br>ultimo id insertado : \$ultimoIdInsertado";
     $rSbN->puntoDeEntrada (
         '<div class="container">'.
             '<div id="main" class="container">'.$message.'</div>'.
