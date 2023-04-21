b288299f277f6771106f6d16923166a461f76e9e
MyName2Ids <r.gg.3@outlook.com>
2023-04-21 09:50:05 -0600

    footer y header

--- ramiroSbackN/index.php
+++ ramiroSbackN/index.php
@@ -1,14 +1,21 @@
 <?php
     require_once './ramiroSbackN.php';
     $rSbN = new ramiroSbackN;
-    /*$rSbN->conectar();
-    $filasAfectadasAlInsertar = $rSbN->nonQuery("INSERT INTO kftable_topics 
-    (topic)VALUES('new topic family kids')");
-    $ultimoIdInsertado = $rSbN->nonQueryId("INSERT INTO kftable_topics 
-    (topic)VALUES('new topic family kids')");*/
+    /*
+        $rSbN->conectar();
+        $filasAfectadasAlInsertar = $rSbN->nonQuery("INSERT INTO kftable_topics 
+        (topic)VALUES('new topic family kids')");
+        $ultimoIdInsertado = $rSbN->nonQueryId("INSERT INTO kftable_topics 
+        (topic)VALUES('new topic family kids')");
+    */
     $message = "hello world!<br>filas afectadas al insertar : 
     \$filasAfectadasAlInsertar<br>ultimo id insertado : \$ultimoIdInsertado";
     $rSbN->puntoDeEntrada (
+        '<div class="container">
+                <header>
+                    <h1><a href="./">ramiroSbackN</a></h1>
+                </header>
+        </div>'.
         '<div class="container">'.
             '<div id="main" class="container">'.$message.'</div>'.
             $rSbN->botonGrisConEfectoHover(
@@ -17,6 +24,11 @@
                 ' - obtener topics <i class="fa-solid fa-fire"></i>'
             ).
         '</div>'.
+        '<div class="container">
+                <footer>
+                    <h1><a href="./">ramiro 2(g) &copy; 2023.</a></h1>
+                </footer>
+        </div>'.
         '<script src="./javascript.js"></script>'
     );
 ?>
