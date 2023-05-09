e6d049612e707ae95aa4d07591cad863a6ce4fac
MyName2Ids <r.gg.3@outlook.com>
2023-05-09 09:03:32 -0600

    cerrando este episodio. para iniciar como debe de ser viejo jajaja ajua!

--- ramiroSbackN/topics.php
+++ ramiroSbackN/topics.php
@@ -8,12 +8,24 @@
 
         $postBody = file_get_contents ("php://input");
         $datarray = $rSbN->login($postBody);
-        header ("Content-Type: application/json");        
-        print_r (json_encode($datarray));
+        header ("Content-Type: application/json"); 
+        if (isset($datarray["result"]["error_id"])) {
+            $responseCode = $datarray["result"]["error_id"];
+            http_response_code($responseCode);
+        } else {
+            http_response_code(200);
+        }
+        echo json_encode($datarray);
 
     } else {
+        
+        header ("Content-Type: application/json");
+        $datarray = $rSbN->error_405 ();
+        echo json_encode ($datarray);
 
-        #print_r ( $rSbN->getData ("SELECT * FROM kftable_topics") );
+    }
+
+    if ( $_SERVER['REQUEST_METHOD'] == "GET" ) {
         $data = $rSbN->getData ("SELECT * FROM kftable_topics");
         for ($i = 0; $i < count($data); $i++) {
             echo "<div class='container noBorder container__' style='width:30%;'><div class='card'>
@@ -21,7 +33,6 @@
             <div class='contenido container__'>".$data[$i]['topic']."</div>
             </div></div>";
         }
-
     }
 
 ?>
